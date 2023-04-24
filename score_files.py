b8e4ac830793d8adb6c6a4ad4e4090d1d96f5857
Rodrigo Chousal Cantú <rodrigo@Rodrigos-MBP.wireless.yale.internal>
2023-04-24 11:48:20 -0400

    Added options for r2r and p2r feature analysis

--- score_files.py
+++ score_files.py
@@ -27,6 +27,9 @@ parser.add_argument("audio_list", help="path to file containing list of audio fi
 parser.add_argument("--window_size", type=int, help="size of utterance frame, adjusts analysis fidelity (default 5s)")
 for label in FEATURE_LABELS:
     parser.add_argument(f"--{label}", help=f"analyze {label} mirroring in the conversation", action='store_true')
+parser.add_argument("--transcription", help="print a transcription of speaker, value, length for each utterance in conversation", action='store_true')
+parser.add_argument("--r2r", help="score speakers on response to response", action='store_true')
+parser.add_argument("--p2r", help="score speakers on prompt to response", action='store_true')
 args = parser.parse_args()
 
 # read in the list of file paths from the command line file
@@ -99,13 +102,100 @@ for key, matrix_object in utterance_matrices.items():
                 loudest_utterances[i] = u
             elif loudest_utterances[i].value == u.value == 0:
                 # capture silence
+                # TODO: What utterance prompts or responds to silence? Is this useful? <---
                 loudest_utterances[i] = Utterance(0, -1, loudest_utterances[i].start_time, loudest_utterances[i].end_time)
     conversation_length = len(loudest_utterances)*window_size
     conversation = Conversation(conversation_length, loudest_utterances, window_size)
     conversation.summarize_speakers()
     conversations.append(conversation)
 
-for c in conversations:
-    c.print_description()
+# print transcription if argument was passed
+if args.transcription:
+    for c in conversations:
+        c.print_description()
 
-# calculate mirroring score for each speaker in each conversation
+# calculate scores for each speaker
+
+# TODO: These could probably more easily be properties 
+# (relative change of utterance value compared to previous speaker's utterance)
+
+# analyze conversation w/o silences (significant utterances)
+sig_utterances = []
+for u in conversation.utterances:
+    if u.speaker_id != -1:
+        sig_utterances.append(u)
+
+if args.p2r:
+    # Calculate average feature ratio of reply:prompt for each utterance
+    speaker_p2r_ratios = {}
+    # For each utterance
+    for i, u in enumerate(conversation.utterances):
+        # Ignore silence or first utterance
+        if (u.speaker_id == -1) or (i == 0): continue
+        # Find previous non-zero utterance
+        prev_nz_value = -1
+        for j in range(i-1, 0, -1):
+            jth_value = conversation.utterances[j].value
+            if (jth_value > 0):
+                prev_nz_value = jth_value
+                break
+        if (prev_nz_value == -1): continue
+        # Calculate average feature ratio for reply:prompt non-zero utterance
+        sid = u.speaker_id
+        p2r = prev_nz_value/u.value
+        if sid in speaker_p2r_ratios:
+            speaker_p2r_ratios[sid].append((p2r, u.length))
+        else:
+            speaker_p2r_ratios[sid] = [(p2r, u.length)]
+    for key, ratios in speaker_p2r_ratios.items():
+        sum_of_ratios = 0
+        for ratio in ratios:
+            sum_of_ratios += ratio[0]
+        average_ratio = sum_of_ratios/len(ratios)
+        print(f"Speaker {key} average P2R ratio: {average_ratio:.4f}")
+if args.r2r:
+    # Calculate relative change in average feature value for same speaker's responses
+    # compared to the prompter's relative change
+    speaker_r2r_ratios = {}
+    conversation_rr_ratios = []
+    # For each significant utterance
+    for i, u in enumerate(sig_utterances):
+        # Ignore first and second utterances
+        if (i < 3): continue
+        # Find same speaker's previous utterance
+        sid = u.speaker_id
+        curr_value_0 = u.value
+        prev_value_0 = -1 # this could be u.prev.value
+        for j in range(i-1, 0, -1):
+            jth_value = sig_utterances[j].value
+            jth_id = sig_utterances[j].speaker_id
+            if (jth_id == sid):
+                prev_value_0 = jth_value
+                break
+        if (prev_value_0 == -1.0): continue
+        # Find different speaker's previous utterance
+        prompt_utterance = sig_utterances[i-1]
+        sid = prompt_utterance.speaker_id
+        curr_value_1 = prompt_utterance.value
+        prev_value_1 = -1 # this could be u.prev.value
+        for j in range(i-2, 0, -1):
+            jth_value = sig_utterances[j].value
+            jth_id = sig_utterances[j].speaker_id
+            if (jth_id == sid):
+                prev_value_1 = jth_value
+                break
+        if (prev_value_1 == -1.0): continue
+        # Calculate r2r
+        speaker_change = curr_value_0/prev_value_0
+        prompter_change = curr_value_1/prev_value_1
+        r2r = speaker_change/prompter_change
+        if sid in speaker_r2r_ratios:
+            speaker_r2r_ratios[sid].append((r2r, u.length))
+        else:
+            speaker_r2r_ratios[sid] = [(r2r, u.length)]
+    for key, ratios in speaker_r2r_ratios.items():
+        sum_of_ratios = 0
+        for ratio in ratios:
+            sum_of_ratios += ratio[0]
+        average_ratio = sum_of_ratios/len(ratios)
+        print(f"Speaker {key} average R2R ratio: {average_ratio:.4f}")
